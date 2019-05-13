6ad5afcee688fecc32d47026d9986549f5d605d7
田上隼人 <a.i.answer@icloud.com>
2019-05-13 17:43:54 +0900

    fizz buzzの出力をするようにしむけた

--- fizz buzz.py
+++ fizz buzz.py
@@ -2,10 +2,12 @@ number = int(input("一つの自然数を入れてね:"))
 
 if number % 3 == 0:
     output = "Fizz"
+
+elif number % 5 == 0:
+    output = "Buzz"
+elif number % 15 == 0:
+    output = "FizzBuzz"
 else:
     output = str(number)
-elif:
-number % 5 == 0:
-output = "Buzz"
 
 print(output)
