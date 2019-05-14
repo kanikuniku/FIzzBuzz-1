742c8d5d148688bb9ca2ad29f91cb9e70a4928b3
田上隼人 <a.i.answer@icloud.com>
2019-05-14 13:09:53 +0900

    outputをprintに変更した
    見やすさ重視で書いていくといい

--- fizz buzz.py
+++ fizz buzz.py
@@ -1,13 +1,13 @@
 number = int(input("一つの自然数を入れてね:"))
 
 if number % 15 == 0:
-    output = "FizzBuzz"
+    print("FizzBuzz")
 
 elif number % 5 == 0:
-    output = "Buzz"
+    print("Buzz")
 elif number % 3 == 0:
-    output = "Fizz"
+    output("Fizz")
 else:
     output = str(number)
 
-print(output)
+print(str(number))
