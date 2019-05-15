bde4c21883518d5ded0226701804c8eee45c6a3b
田上隼人 <a.i.answer@icloud.com>
2019-05-15 15:15:15 +0900

    commit first

--- fizz buzz.py
+++ fizz buzz.py
@@ -1,11 +1,12 @@
 def fizzbuzz_convert(number):
     pass
 
+
 if number % 15 == 0:
     return "FizzBuzz"
 
     if number % 3 == 0:
-    return "Fizz"
+        return "Fizz"
     if number % 5 == 0:
         return "Buzz"
 
